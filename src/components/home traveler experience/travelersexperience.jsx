8c246bbc6f4fe564282cb57c20a3f3b3b957c500
SasithDuleepa <duleepajayathissa@gmail.com>
2023-10-25 17:35:51 +0530

    traveler experience

--- src/components/home traveler experience/travelersexperience.jsx
+++ src/components/home traveler experience/travelersexperience.jsx
@@ -6,106 +6,115 @@ import review from './../../assets/review.png';
 export default function Travelersexperience() {
     const [activeSlide, setActiveSlide] = useState(0);
   return (
-    <div>
-        <h1 className='travelerexperience-title'>Traveler Experience</h1>
+    <div className='traveler_experience'>
+      <div className='traveler-experience'>
+      
+      <h1 className='travelerexperience-title'>Traveler Experience</h1>
 
-       
-       <Carousel
-        containerProps={{
-          
-          style: {
-            width: "100%",
-            justifyContent: "space-between",
-            userSelect: "none"
-          }
-        }}
-        preventScrollOnSwipe
-        swipeTreshold={60}
-        activeSlideIndex={activeSlide}
-        // activeSlideProps={{
-        //   style: {
-        //     background: "blue"
-        //   }
-        // }}
-        onRequestChange={setActiveSlide}
-        forwardBtnProps={{
-          children: ">",
-          
-          style: {
-            width: 60,
-            height: 60,
-            minWidth: 60,
-            alignSelf: "center",
-            borderRadius:"100px",
-            border:"none"
-           
-          }
-        }}
-        backwardBtnProps={{
-          children: "<",
-          style: {
-            width: 60,
-            height: 60,
-            minWidth: 60,
-            alignSelf: "center",
-            borderRadius:"100px",
-            border:"none",
-            marginRight:"50px",
-            
-          }
-        }}
-        dotsNav={{
-          show: false,
-          itemBtnProps: {
-            style: {
-              height: 16,
-              width: 16,
-              borderRadius: "50%",
-              border: 0,
-              marginLeft:"50px",
-            }
-          },
-          activeItemBtnProps: {
-            style: {
-              height: 16,
-              width: 16,
-              borderRadius: "50%",
-              border: 0,
-              background: "black"
-            }
-          }
-        }}
+     
+     <Carousel
+      containerProps={{
         
-        autoplay={true}
-        delay={1000}
-        itemsToShow={1}
-        speed={1600}
-        easing="ease-in-out"
-        centerMode
-      >
-        {/* {Array.from({ length: 10 }).map((item, index) => (
-          <div
-            style={{
-              background: "yellow",
-              width: 300,
-              height: 300,
-              border: "30px solid white",
-              textAlign: "center",
-              lineHeight: "240px",
-              boxSizing: "border-box"
-            }}
-            key={index}
-          >
-            {index}
-          </div>
-        ))} */}
+        style: {
+          width: "100%",
+          justifyContent: "space-between",
+          userSelect: "none"
+        }
+      }}
+      preventScrollOnSwipe
+      swipeTreshold={60}
+      activeSlideIndex={activeSlide}
+      // activeSlideProps={{
+      //   style: {
+      //     background: "blue"
+      //   }
+      // }}
+      onRequestChange={setActiveSlide}
+      forwardBtnProps={{
+        children: ">",
+        
+        style: {
+          width: 60,
+          height: 60,
+          minWidth: 60,
+          alignSelf: "center",
+          borderRadius:"100px",
+          border:"none",
+          background:"rgba(97, 97, 97, 0.214",
+         
+        
+         
+        }
+      }}
+      backwardBtnProps={{
+        children: "<",
+        style: {
+          width: 60,
+          height: 60,
+          minWidth: 60,
+          alignSelf: "center",
+          borderRadius:"100px",
+          border:"none",
+          marginRight:"00px",
+          
+          background:"rgba(97, 97, 97, 0.214"
+          
+        }
+      }}
+      dotsNav={{
+        show: false,
+        itemBtnProps: {
+          style: {
+            height: 16,
+            width: 16,
+            borderRadius: "50%",
+            border: 0,
+            marginLeft:"50px",
+          }
+        },
+        activeItemBtnProps: {
+          style: {
+            height: 16,
+            width: 16,
+            borderRadius: "50%",
+            border: 0,
+            background: "black"
+          }
+        }
+      }}
+      
+      autoplay={true}
+      delay={1000}
+      itemsToShow={1}
+      speed={1600}
+      easing="ease-in-out"
+      centerMode
+    >
+      {/* {Array.from({ length: 10 }).map((item, index) => (
+        <div
+          style={{
+            background: "yellow",
+            width: 300,
+            height: 300,
+            border: "30px solid white",
+            textAlign: "center",
+            lineHeight: "240px",
+            boxSizing: "border-box"
+          }}
+          key={index}
+        >
+          {index}
+        </div>
+      ))} */}
 
 <Travelerexperience/>
 <Travelerexperience/>
 <Travelerexperience/>
 <Travelerexperience/>
-      </Carousel>
+    </Carousel>
+  </div>
     </div>
+    
   )
 }
 function Travelerexperience(){
@@ -127,7 +136,7 @@ function Travelerexperience(){
                    quisque pulvinar fusce. Eget eu dictum eu mattis 
                    etiam aliquam venenatis feugiat.
                  Nunc libero viverra natoque diam at.</p>
-                 <p>Date </p>
+                 <p className='travelerexperience-container-right-text'>Date </p>
         </div>
     </div>
     )
