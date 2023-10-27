2328b23db412a8f7cdf79999ac36e1b885f528d5
SasithDuleepa <duleepajayathissa@gmail.com>
2023-10-27 10:29:11 +0530

    add day tour place add

--- src/pages/dashboad/tour packages/tourPackages.jsx
+++ src/pages/dashboad/tour packages/tourPackages.jsx
@@ -3,37 +3,53 @@ import './tourpackages.css';
 import axios from 'axios';
 
 export default function TourPackages() {
-  const [placeResult, setPlaceResult] = useState([]);
-  const [days, setDays] = useState([{ day: 'day 1', place: ['place 1'] },{ day: 'day 2', place: ['place 2'] }]);
-  const [places, setPlaces] = useState([{dayindex:1,places:[]}]);
+  const [dayData, setDayData] =useState([
+    {day:1 ,
+     places:[{
+      placeName:"",
+      placeId:""}]
+    }
+  ])
 
-  const placeSearch = async (e) => {
-    let placeInput = e.target.value;
-    const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/places/placesearch/${placeInput}`);
-    setPlaceResult(res.data.data);
-  };
+  const[dayDataIndex, setDayDataIndex] = useState(0)
 
-  const AddDays = () => {
-    setDays([...days, { day: 'day ' + (days.length + 1), place: [] }]);
-  };
+  const[searchData, setSearchData] = useState([])
 
-  const dayHandler = (index) => () => {
-    console.log(index);
-    setPlaces([...places, {dayindex:index,places:[]}]);
+  //add day
+  const AddDay =()=>{
+    setDayData([...dayData,{day:dayData.length+1,places:[{placeName:null,placeId:null}]}])
   }
 
-  const placeSelectHandler = (index, place) => () => {
-    console.log(index, place);
-    if (placeResult[index]) {
-      const updatedDays = [...days];
-      updatedDays[index].place.push(place);
-      setDays(updatedDays);
-    }
-  };
 
-  const add = () => {
-    console.log(days);
-  };
+  //set data index
+  const setDayDataIndexFunc =(index)=>()=>{
+    console.log(index);
+    setDayDataIndex(index)
+  }
+
+  //search bar
+  const SearchHandler =async(e)=>{
+    console.log(e.target.value);
+    if(e.target.value!==""){
+      const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/places/placesearch/${e.target.value}`)
+      // console.log(res.data.data);
+      setSearchData(res.data.data)
+    }else{
+      setSearchData([])
+    }
+  }
+
+
+  //add places to a day
+  const SelectPlace =(place_name,placeId)=>()=>{
+    console.log(place_name, placeId);
+    const newDayData = [...dayData]
+    newDayData[dayDataIndex].places.push({placeName:place_name,placeId:placeId})
+    setDayData(newDayData)
+    // console.log(newDayData[dayDataIndex].places)
+
+  }
+
 
   return (
     <div>
@@ -58,42 +74,42 @@ export default function TourPackages() {
 
       <div className='tour-package-main-2'>
         <div className='tour-package-days-div'>
-          {days.map((day, index) => (
-            <a className='day' onClick={dayHandler(index)} key={index}>
-              {day.day}
-              {index + 1}
-            </a>
-          ))}
-          <a className='day' onClick={AddDays}>
+          {dayData.map((item,index)=>{
+            return(
+              <a className='day' onClick={setDayDataIndexFunc(index) }>day {index+1}</a>
+            )
+          })}
+          
+          <a className='day' onClick={AddDay} >
             add +
           </a>
         </div>
         <div className='tour-package-place-div'>
           <div>
             <label>place:</label>
-            <input type="text" onChange={(e) => placeSearch(e)} />
+            <input type="text" onChange={(e)=>SearchHandler(e)}/>
           </div>
-          {placeResult && placeResult.length > 0 ? (
-            placeResult.map((place, index) => (
-              <ol key={index}>
-                <li>
-                  <a onClick={placeSelectHandler(index, place.place_name)}>{place.place_name}</a>
-                </li>
-              </ol>
-            ))
-          ) : (
-            <p>no results</p>
+          {searchData.map((place,index)=>{
+            return(
+              <div>
+                <a onClick={SelectPlace(place.place_name,place.place_id)}>{place.place_name}</a>
+              </div>
+            )
+          
+          })}
+
+          <h1>selected places</h1>
+          {dayData[dayDataIndex].places.map((item,index)=>{
+            return(
+              <div>
+                <a>{item.placeName}</a>
+              </div>
           )}
-          {/* {places.length > 0 ? (
-            places.map((place, index) => (
-              <div key={index}>{place}</div>
-            ))
-          ) : (
-            <p>not selected</p>
-          )} */}
+          )}
+          
         </div>
       </div>
-      <a onClick={add}>click</a>
+      <a >click</a>
     </div>
   );
 }
