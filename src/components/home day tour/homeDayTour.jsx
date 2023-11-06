05bd12412611ddbd122fc02a71a1d5e5ab7bd35a
SasithDuleepa <duleepajayathissa@gmail.com>
2023-11-06 16:30:50 +0530

    edite carousel

--- src/components/home day tour/homeDayTour.jsx
+++ src/components/home day tour/homeDayTour.jsx
@@ -7,34 +7,86 @@ import Arrow from './../../assets/icons/arrow-right.png';
 import Sinharaja from './../../assets/Sinharaja.png'
 
 export default function HomeDayTour() {
+  const [activeSlide, setActiveSlide] = useState(0);
+  const [activeSlide_, setActiveSlide_] = useState(0);
+  const activeslideHandler = (val) =>{
+    setActiveSlide(val)
+      console.log('active val',val);
+      setTimeout(() => {
+        setActiveSlide(val)
+      },0);
+      setTimeout(() => {
+        setActiveSlide_(val)
+      },2000);
+  }
   const [daytours,setDaytours] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
   // const [daytours,setDaytours] = useState([{day_tour:"",day_tour_id:"",description:"",img:"",price:"",_id:""}]);
   const GetDayTours = async() =>{
     const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/daytour/daytours`);
     // console.log(res.data);
     setDaytours(res.data);
     // console.log(daytours)
+    
   }
   useEffect(()=>{GetDayTours()
   
 },[])
 
   useEffect(() => {
-    console.log(daytours, activeSlide); // Log the updated daytours state here
-  }, [daytours]);
+    console.log(daytours, activeSlide);
+    console.log(daytours[activeSlide])
+    setIsLoaded(false);
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000);
+  }, [daytours, activeSlide]);
 
 
-  const [activeSlide, setActiveSlide] = useState(0);
+  
   return (
     <div className='HomeDayTour'>
       <div className='HomeDayTour-main'>
         
-        <div className='HomeDayTour-carousel-info'>
+        
+
+          
+          {daytours.length > 0 ?
+          <div className='HomeDayTour-carousel-info'>
+          <h1 className='HomeDayTour-carousel-info-h1'>Day Tour Packages</h1>
+          <p className={`HomeDayTour-carousel-info-p1 ${isLoaded ? 'start-animation' : ''}`}>{daytours[activeSlide_].day_tour}</p>
+          <p className={`HomeDayTour-carousel-info-p2 ${isLoaded ? 'start-animation' : ''}`}>{daytours[activeSlide_].description}</p>
+          
+             <div className='HomeDayTour-carousel-info-price-div'>
+             <p className={`HomeDayTour-carousel-price-p ${isLoaded ? 'start-animation' : ''}`}>price :</p>
+              
+              <p className={`HomeDayTour-carousel-price ${isLoaded ? 'start-animation' : ''}`}>${daytours[activeSlide_].price}</p>
+
+             </div>
+             <a className={`HomeDayTour-carousel-readmore ${isLoaded ? 'start-animation' : ''}`}>Read more...</a>
+            
+          </div>
+          :null
+          }
+          
+
+             
+
+        
+
+
+
+
+
+
+
+        {/* <div className='HomeDayTour-carousel-info'>
 
           <h1 className='HomeDayTour-carousel-info-h1'>Day Tour Packages</h1>
-          {/* {daytours.length > 0 ?
-          <p className='HomeDayTour-carousel-info-p1'>{daytours[activeSlide].day_tour}</p>:null
-          } */}
+          {daytours.length > 0 ?
+          <p className={`HomeDayTour-carousel-info-p1 ${isLoaded ? 'start-animation' : ''}`}>{daytours[activeSlide].day_tour}</p>
+          :null
+          }
           
           <p className='HomeDayTour-carousel-info-p2'>Lorem ipsum dolor sit amet consectetur. 
             Pellentesque enim vestibulum sapien pellentesque
@@ -50,7 +102,19 @@ export default function HomeDayTour() {
              </div>
              <a className='HomeDayTour-carousel-readmore'>Read more...</a>
 
-        </div>
+        </div> */}
+
+
+
+
+
+
+
+
+
+
+
+
         <div>
         <Carousel
         containerProps={{
@@ -61,9 +125,9 @@ export default function HomeDayTour() {
         }}
         // preventScrollOnSwipe
         swipeTreshold={10}
-        activeSlideIndex={activeSlide}
+        activeSlideIndex={activeSlide_}
 
-        onRequestChange={setActiveSlide}
+        onRequestChange={activeslideHandler}
         forwardBtnProps={{
           children: <img className="arrow-right" src={Arrow} />,
           className: "HomeDayTour-carousel-forward-btn",
@@ -95,9 +159,9 @@ export default function HomeDayTour() {
         }}
         
         autoplay={true}
-        delay={1000}
+        delay={2000}
         itemsToShow={1}
-        speed={1900}
+        speed={1000}
         itemsToScroll={1}
         // autoplayDirection="backward"
         // easing="ease-in-out"
@@ -105,12 +169,13 @@ export default function HomeDayTour() {
       >
         {daytours.length > 0 && daytours.map((daytour, index) => (
           <img key={index} src={Sinharaja} alt="" className='DayTourCarousel-img'/>
+          // <div className='DayTourCarousel-img'>{daytour._id}</div>
         ))
           }
 
 
  
-        <img src={Sinharaja} alt="" className='DayTourCarousel-img'/>
+        {/* <img src={Sinharaja} alt="" className='DayTourCarousel-img'/> */}
         
       </Carousel>
         </div>
